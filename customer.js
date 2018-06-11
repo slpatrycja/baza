282de19ba72f201d23bf0f17d55fbd689dfa7ef8
slpatrycja <sl.patrycja.of@gmail.com>
2018-06-11 19:31:35 +0200

    added classes to js file

---
+++ customer.js
@@ -0,0 +1,9 @@
+class Customer {
+
+    constructor(arg1, arg2, arg3, arg4) {
+        this.imie = arg1;
+        this.nazwisko = arg2;
+        this.wiek = arg3;
+        this.plec = arg4;
+    }
+}
